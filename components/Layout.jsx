16d2675cfd02d69c10737326241ab9cf15c3f7ad
Daniel Ward <dward1502@gmail.com>
2022-08-13 11:52:41 -0700

    layout of main page and filters page

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,13 +2,11 @@ import React, { Fragment } from 'react';
 import Navigation from './Navigation/Navigation';
 import Footer from './Footer/Footer';
 
-import styles from '../SASS/Home.module.scss';
-
 const Layout = ({ children }) => {
   return (
     <Fragment>
       <Navigation />
-      <main className={styles.content}>{children}</main>
+      <main>{children}</main>
       <Footer/>
     </Fragment>
   );
--- pages/filamentFilters.jsx
+++ pages/filamentFilters.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+
+import styles from './filament.module.scss'
 
 const filamentFilters = () => {
   return (
-    <div>filamentFilters</div>
+    <div className={styles.main}>
+      <section className={styles.filters}>
+        <h1>Basic Filters</h1>
+        <h3>Filter by Manufacturer</h3>
+        <h3>Filament Type</h3>
+        <h3>Color / Appearance</h3>
+        <h3>Location</h3>
+        <h3>Size</h3>
+      </section>
+      <section className={styles.filaments}>
+
+      </section>
+      <aside className={styles.addSpace}>
+
+      </aside>
+    </div>
   )
 }
 
